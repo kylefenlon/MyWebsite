acf49bc2c007177802605a35f48d1e0c09e64109
kylefenlon <kyle@kfenlon.com>
2023-01-15 15:03:11 +0000

    changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -21,18 +21,18 @@ const NavBar = () => {
 
     return (
         <div class={colour ? "header header-bg" : "header"}>
-            <Link to="/">
+            <Link to="/MyWebsite">
                 <div class="main-title">KFenlon.com</div>
             </Link>
             <ul class="nav-items">
                 <li>
-                    <Link to="/MyWebsite">Home</Link>
+                    <Link to="/MyWebsite/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/projects">Projects</Link>
+                    <Link to="/MyWebsite/projects">Projects</Link>
                 </li>
                 <li>
-                    <Link to="/blog">Blog</Link>
+                    <Link to="/MyWebsite/blog">Blog</Link>
                 </li>
                 {/* <li>
                     <Link to="/contact">Contact</Link>
