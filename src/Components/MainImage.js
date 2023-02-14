86f4d03ad13fc421169b9ff6ab0ffd520006632b
kylefenlon <kyle@kfenlon.com>
2023-02-14 14:28:04 +0000

    fixed into title

--- src/Components/MainImage.js
+++ src/Components/MainImage.js
@@ -12,7 +12,7 @@ const MainImg = () => {
             </div>
             <div class="content">
                 <p>Hi, my name is Kyle Fenlon.</p>
-                <h1>Software Devloper</h1>
+                <h1>Software Developer</h1>
                 <div>
                     <Link to="/projects" class="btn">Projects</Link>
                     <Link to="/blog" class="btn-light">Blog</Link>
