3ba20eb0f421ae451262a3105fc9f822a064397b
kylefenlon <kyle@kfenlon.com>
2024-05-26 21:08:00 +0100

    added navbar MUI

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react"
-import React from 'react'
-import { Link } from "react-router-dom"
-import { AppBar } from "@mui/material"
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
@@ -9,14 +8,12 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
-import App from "../App"
-import CssBaseline from "@mui/material/CssBaseline";
+import App from '../App';
 
 const NavBar = () => {
-
     const [click, setClick] = useState(false);
-    const [colour, setColour] = useState(false)
-    const [anchorEl, setAnchorEl] = useState(null);
+    const [colour, setColour] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(false);
 
     const handleClick = () => setClick(!click);
     const handleMenuOpen = (event) => setAnchorEl(event.currentTarget);
@@ -24,67 +21,56 @@ const NavBar = () => {
 
     const changeColour = () => {
         if (window.scrollY >= 100) {
-            setColour(true);
+            setColour(true)
         } else {
             setColour(false)
         }
-    };
+    }
+
     useEffect(() => {
-        window.addEventListener("scroll", changeColour);
+        window.addEventListener("scroll", changeColour)
         return () => {
-            window.removeEventListener("scroll", changeColour);
-        };
-    }, []);
+            window.removeEventListener("scroll", changeColour)
+        }
+    }, [])
 
     return (
-        <>
-            {/* <CssBaseline /> */}
-            <AppBar position="fixed" sx={{
-                background: colour
-                    ? 'transparent'
-                    : 'linear-gradient(to bottom right, #09203f 0%, #537895 100%)',
-                transition: 'background-color 0.5s ease'
-            }}>
-                <Toolbar>
-                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-                        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-                            KFenlon.com
-                        </Link>
-                    </Typography>
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <MenuItem sx={{ fontSize: '1.25rem' }} component={Link} to="/">Home</MenuItem>
-                        <MenuItem sx={{ fontSize: '1.25rem' }} component={Link} to="/projects">Projects</MenuItem>
-                        <MenuItem sx={{ fontSize: '1.25rem' }} component={Link} to="/blog">Blog</MenuItem>
-                    </Box>
-                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ ml: 2 }}
-                            onClick={handleMenuOpen}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl)}
-                            onClose={handleMenuClose}
-                        >
-                            <MenuItem component={Link} to="/" onClick={handleMenuClose}>Home</MenuItem>
-                            <MenuItem component={Link} to="/projects" onClick={handleMenuClose}>Projects</MenuItem>
-                            <MenuItem component={Link} to="/blog" onClick={handleMenuClose}>Blog</MenuItem>
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </>
+        <AppBar position="fixed" sx={{ backgroundColor: colour ? 'primary.main' : 'transparent', transition: 'background-color 0.5s ease' }}>
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+                        KFenlon.com
+                    </Link>
+                </Typography>
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    <MenuItem component={Link} to="/">Home</MenuItem>
+                    <MenuItem component={Link} to="/projects">Projects</MenuItem>
+                    <MenuItem component={Link} to="/blog">Blog</MenuItem>
+                </Box>
+                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ ml: 2 }}
+                        onClick={handleMenuOpen}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                    >
+                        <MenuItem component={Link} to="/" onClick={handleMenuClose}>Home</MenuItem>
+                        <MenuItem component={Link} to="/projects" onClick={handleMenuClose}>Projects</MenuItem>
+                        <MenuItem component={Link} to="/blog" onClick={handleMenuClose}>Blog</MenuItem>
+                    </Menu>
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
-
 }
-
-
-
 
 export default NavBar;
