1a99016c0d753ba44c6f8fa0c589233912722258
kylefenlon <kyle@kfenlon.com>
2024-05-27 23:56:25 +0100

    almost finished the about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,22 +1,56 @@
 import React from "react";
+import ProfilePhoto from '../images/Profile-photo.png'
+import DadGolf from '../images/Dad-Golf.png'
+import SnowyMunro from '../images/Snowy-Munro.png'
+import SunnyMunro from '../images/Sunny-Munro.png'
+import TechTalk from '../images/Tech-Talk.png'
 import '../CSS/About.css'
+
 
 const About = () => {
     return (
-        <div className="mainContainer">
-            <div className="left">
-                <div className="heading">
-                    <h1 className="mainHeadingAbout">about.</h1>
+        <div className="topAboutContainer">
+            <div className="mainContainer">
+                <div className="leftAbout">
+                    <div className="heading">
+                        <h1 className="mainHeadingAbout">
+                            about.
+                        </h1>
+                    </div>
+                    <div className="intro">
+                        <p className="firstPara">
+                            I'm a Software Developer from<br></br>
+                            Glasgow, Scotland.
+                        </p>
+                    </div>
+                    <div className="descriptionAbout">
+                        <p className="secondPara">
+                            Banging my head against the wall trying to
+                            solve a coding issue is my idea of fun. However I guess for 'fun',
+                            you will also find me golfing, working out, seeing friends/family
+                            and travelling.
+                        </p>
+                    </div>
                 </div>
-                <div className="intro">
-                    <p className="firstPara">Hello there!</p>
-                </div>
-                <div className="description">
-                    <p className="secondPara">Hello there!</p>
+                <div className="rightAbout">
+                    <img src={ProfilePhoto} alt='Profile' className="profilePhoto" />
                 </div>
             </div>
-            <div className="right">
-                {/* insert image */}
+            <div className="imagesContainer">
+                <ul className="imageList">
+                    <li className="image">
+                        <img src={DadGolf} alt='display' className="smallImage" />
+                    </li>
+                    <li className="image">
+                        <img src={SnowyMunro} alt='display' className="smallImage" />
+                    </li>
+                    <li className="image">
+                        <img src={SunnyMunro} alt='display' className="smallImage" />
+                    </li>
+                    <li className="image">
+                        <img src={TechTalk} alt='display' className="smallImage" />
+                    </li>
+                </ul>
             </div>
         </div>
     )
