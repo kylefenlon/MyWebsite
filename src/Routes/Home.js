464e4167ce5cec60c5824d9a98557305e5ec0e55
kylefenlon <kyle@kfenlon.com>
2024-06-03 14:54:03 +0100

    fixed the tweaks

---
+++ src/Routes/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from '../Components/NavBar';
+import HomeContainer from '../Containers/HomeContainer'
+import '../CSS/Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+            <div className='pageContent'>
+                <HomeContainer />
+            </div>
+        </div>
+    )
+};
+
+export default Home;
